9dd3b9895fd7fd613c06d68d7af29eec95b404d8
Wellington Menezes <ligton.ribeiro@gmail.com>
2022-09-28 12:54:00 -0300

    chore: instalação TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
   controllers: [],
   providers: [],
 })
