59a27920099cb2487cf17a9e06896ccb985aba6e
Wellington Menezes <ligton.ribeiro@gmail.com>
2022-10-22 08:26:09 -0300

    fix: adicionado UseGuard na rota GET/:id da instituição

--- src/modules/institution/controller/get-one-institution.controller.ts
+++ src/modules/institution/controller/get-one-institution.controller.ts
@@ -4,14 +4,20 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import {
   IFindAllInstitution,
   GetOneInstitutionService,
 } from '@modules/institution/services';
 import { GetAllInstitutionDto } from '../dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Instituição')
 @Controller('api/v1/institution')
@@ -25,6 +31,8 @@ export class GetOneInstitutionController {
     summary: 'Retorna uma Instituição cadastrada na plataforma',
   })
   @ApiResponse({ status: 200, type: GetAllInstitutionDto })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
   async findOne(@Param('id') id: string): Promise<IFindAllInstitution> {
     return await this.getOneInstitutionService.findById(id);
